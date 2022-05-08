d16a2580ccd84ddd876a2132d81f6ef945f4a0f6
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2022-05-08 19:11:41 -0400

    Started playing with different syntax for IF/SWITCH/LOOPS .. Xcode git interface is NOT fun so far

---
+++ swift-playground-00.playground/Pages/Control-Flows.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+// I don't need to use parenthesis on IF statements:
+// I always have to use curly brackets, EVEN if it's a one line statement
+var apple:Int = 123
+
+if apple == 123 {
+    print("Cool beans")
+}
+
+// Non-True/False conditions are NOT allowed
+//if apple {
+//    print("Non-True/False conditions are NOT allowed")
+//}
