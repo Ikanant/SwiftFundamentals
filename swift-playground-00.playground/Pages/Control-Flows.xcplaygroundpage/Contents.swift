abf028ab2ad6de44e2d47c8ca5aa1f070c169ade
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2022-05-08 19:36:40 -0400

    Loops have a weird syntax

--- swift-playground-00.playground/Pages/Control-Flows.xcplaygroundpage/Contents.swift
+++ swift-playground-00.playground/Pages/Control-Flows.xcplaygroundpage/Contents.swift
@@ -19,10 +19,28 @@ case 0:
 case 1:
     print("ONE")
     break // NOT REQUIRED, BUT ALLOWED
+// Note1: Ranges are allowed!
+// Note2: Like other languages, even though you might have 2 cases that are valid,
+// ONLY the first found will run... in this case I will print "I can use Ranges"
+case 100...200:
+    print("I can use RANGES")
 case 123:
     print("ONE TWO THREE but NOT FOUR")
 case 111,222,333: // This is the way to deal with fall through cases
     break
 default:
     print("DEFAULT VALUE 😀")
+}
+
+
+// So weird, loops are pretty straight forward BUT SWIFT does NOT allow for good old for-loops
+
+//for (var i=0; i<10; i++) {
+//    print(i)
+//}
+
+var bunchOfWords: [String] = ["one", "random", "car", "coffee"];
+// Straight forward... but interesting how I was NOT allowed to use parenthesis here
+for word in bunchOfWords {
+    print(word)
 }
