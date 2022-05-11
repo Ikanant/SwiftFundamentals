9e238e59a0e461f92d89a08837a6271ab2faf620
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2022-05-11 19:34:01 -0400

    Added an example how classes and structs are treated differently in Swift. Pass by REFERENCE/VALUE

--- swift-playground-00.playground/Pages/Classes.xcplaygroundpage/Contents.swift
+++ swift-playground-00.playground/Pages/Classes.xcplaygroundpage/Contents.swift
@@ -1,14 +1,32 @@
-class Person {
-    var firstName: String = ""
-    var lastName: String = ""
+class PersonClass {
+    var firstName: String
+    var lastName: String
     
+    init (firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
     func printName() {
         print ("Hi, my name is: \(firstName) \(self.lastName)")
     }
 }
 
-var user = Person()
-user.firstName = "Jonathan"
-user.lastName = "Snow"
-user.printNa me()
-  
+
+struct PersonStruct {
+    var firstName: String
+    var lastName: String
+    func printName() {
+        print ("Hi, my name is: \(firstName) \(self.lastName)")
+    }
+}
+
+var p1 = PersonClass(firstName: "Jon", lastName: "A")
+var s1 = PersonStruct(firstName: "Jon", lastName: "B")
+
+var p2 = p1
+p2.firstName = "Peter"
+print(p1.firstName)
+
+var s2 = s1
+s2.firstName = "Pedro"
+print(s1.firstName)
